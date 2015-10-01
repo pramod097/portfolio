3a318bbc14a5d2ca614f77394be364c9e7ac1664
pramod097 <polisettypramod97@gmail.com>
2015-10-01 18:21:06 +0530

    First Commit

---
+++ js/page.js
@@ -0,0 +1,27 @@
+<script>
+	$(window).load(function() {
+		$("#flexiselDemo1").flexisel({
+			visibleItems: 9,
+			animationSpeed: 1000,
+			autoPlay: true,
+			autoPlaySpeed: 3000,    		
+			pauseOnHover: true,
+			enableResponsiveBreakpoints: true,
+	    	responsiveBreakpoints: { 
+	    		portrait: { 
+	    			changePoint:480,
+	    			visibleItems: 1
+	    		}, 
+	    		landscape: { 
+	    			changePoint:640,
+	    			visibleItems: 2
+	    		},
+	    		tablet: { 
+	    			changePoint:768,
+	    			visibleItems: 3
+	    		}
+	    	}
+	    });
+	    
+	});
+</script>
